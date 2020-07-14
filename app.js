6c832984aa072bf4ece102b008a0cf701d947efb
georgina puig <georginaxcodes@gmail.com>
2020-07-14 01:31:49 -0500

    add home and index routes

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ const blogSchema = new mongoose.Schema({
 });
 const Blog = mongoose.model('Blog', blogSchema);
 
-Blog.create({
-  title: 'Test Blog',
-  image: 'https://images.unsplash.com/photo-1586088209375-7c9f50ff8b5a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
-  body: 'This is a blog post',
-});
+// Blog.create({
+//   title: 'Test Blog',
+//   image: 'https://images.unsplash.com/photo-1586088209375-7c9f50ff8b5a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+//   body: 'This is a blog post',
+// });
 
 // APP CONFIG
 app.use(bodyParser.urlencoded({extended: true})); // body parser
@@ -39,6 +39,22 @@ app.use(express.static); // static files
 app.set('view engine', 'ejs'); // ejs template engine
 
 // RESTFUL ROUTES
+// home
+app.get('/', function(req, res) {
+  res.redirect('/blogs');
+});
+
+// index
+app.get('/blogs', function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log('error');
+    } else {
+      res.render('index', { blogs: blogs });
+    }
+  });
+});
+
 
 // PORT CONFIG
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
